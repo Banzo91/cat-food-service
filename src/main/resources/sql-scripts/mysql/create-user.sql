0dc79a5401f630b0500148b81ea61a2638202bc5
banzo276 <matdudek2706@gmail.com>
2021-04-24 18:19:45 +0200

    Fixed mysql create user script to limit privileges

--- src/main/resources/sql-scripts/mysql/create-user.sql
+++ src/main/resources/sql-scripts/mysql/create-user.sql
@@ -1,5 +1,3 @@
 CREATE USER 'catfoodapp'@'%' IDENTIFIED BY 'catfoodapp';
 
-GRANT ALL PRIVILEGES ON * . * TO 'catfoodapp'@'%';
-
-ALTER USER 'catfoodapp'@'%' IDENTIFIED WITH mysql_native_password BY 'catfoodapp';
+GRANT ALL PRIVILEGES ON cat_food_tracker.* TO 'catfoodapp'@'%';
